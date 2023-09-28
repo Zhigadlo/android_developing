cf717de918478d65a1c80f7f206940f94d203410
Vladislav Zhogol <vzhogol176@gmail.com>
2023-09-28 20:25:40 +0300

    lab1

---
+++ lab1/lab1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace lab1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if(NameLabel.IsVisible)
+			NameLabel.IsVisible = false;
+		else
+			NameLabel.IsVisible = true;
+	}
+}
+
